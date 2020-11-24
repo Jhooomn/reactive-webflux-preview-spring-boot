c7c4536c6dce8a19687cad8ab86fcedc6c3d9243
Jhon Baron <jhon.baron@zabud.com.co>
2020-11-24 15:07:38 -0500

    added: synchronizedObject to be carefully about incremental numberOfRequest variable.

--- src/main/java/com/app/reactortest/interfaces/PersonController.java
+++ src/main/java/com/app/reactortest/interfaces/PersonController.java
@@ -3,6 +3,7 @@ package com.app.reactortest.interfaces;
 import com.app.reactortest.domain.dto.PersonDTO;
 import com.app.reactortest.services.PersonService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +13,19 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/person")
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
+@Slf4j
 public class PersonController {
+  private static volatile int numberOfRequest;
 
   private final PersonService personService;
+  private final Object synchronizedObject = new Object();
 
   @GetMapping(value = "/", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
   public Flux<PersonDTO> listPerson() {
+    synchronized (synchronizedObject) {
+      log.info(
+          "[PersonController Request] ::: #{} :::  [PersonController Request]", numberOfRequest++);
+    }
     return personService.listPerson();
   }
 
